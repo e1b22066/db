f2672da298051ecf46578b2dea55efcb3807a66d
e1b22066 <e1b22066@oit.ac.jp>
2024-10-15 13:47:18 +0900

    初期設定

---
+++ src/main/java/oit/is/z2722/db/DbApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z2722.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z2722/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z2722.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
