58c094e0023041fcfffc15de9eabe84dec1a18cc
e1b22066 <e1b22066@oit.ac.jp>
2024-10-15 15:52:00 +0900

    DBの先頭のデータをSELECTで一つだけ取得して表示

--- src/main/java/oit/is/z2722/db/Controller/Sample41Controller.java
+++ src/main/java/oit/is/z2722/db/Controller/Sample41Controller.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.z2722.db.model.Chamber;
+import oit.is.z2722.db.model.ChamberMapper;
+
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
  */
@@ -20,9 +23,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
     return "sample41.html";
   }
 
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
+    return "sample41.html";
+  }
 }
